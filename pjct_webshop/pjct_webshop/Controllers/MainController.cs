ad45b7e12ffc74dc6718d7fb27334b0d17694956
Olle Svensson <aw.ots@outlook.com>
2015-10-21 10:01:46 +0200

    New solution

---
+++ pjct_webshop/pjct_webshop/Controllers/MainController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pjct_webshop.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Butik()
+        {
+            return View();
+        }
+
+        public ActionResult Info()
+        {
+            return View();
+        }
+    }
+}

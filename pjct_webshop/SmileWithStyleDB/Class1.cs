d459d61c7f3883e3a80bcfcdad156ff66ed4b514
Cecilia Nilsson <c.e.l.nilsson@gmail.com>
2015-10-23 14:06:19 +0200

    SQLConnection

--- pjct_webshop/SmileWithStyleDB/Class1.cs
+++ pjct_webshop/SmileWithStyleDB/Class1.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace SmileWithStyleDB
 {
     public class Class1
     {
+        SqlConnection myConnection = new SqlConnection("Data Source = localhost");
+        private SqlParameter myParam;
+        SqlDataReader myReader = null;
+
+        private void DatabaseConnection()
+        {
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private string GetImages(string input)
+        {
+            var thisImage = "";
+            myParam = new SqlParameter("@Param1", SqlDbType.VarChar, 11);
+            myParam.Value = input;
+
+            try
+            {
+                var myCommand = new SqlCommand(
+                 "SELECT ImagePath FROM Procucts WHERE ArtNumber = @Param1", myConnection);
+                myCommand.Parameters.Add(myParam);
+                while (myReader.Read())
+                {
+                    return myReader["ImagePath"].ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return myReader["ImagePath"].ToString();
+        }
     }
 }

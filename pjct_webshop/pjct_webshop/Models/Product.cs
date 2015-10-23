ba8974e1d19e11bfbc019441f1f0fde9acf2fc66
Olle Svensson <aw.ots@outlook.com>
2015-10-23 16:34:32 +0200

    Added some stuff

---
+++ pjct_webshop/pjct_webshop/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace pjct_webshop.Models
+{
+    public class Product
+    {
+        public List<Product> ProductList = new List<Product>();
+
+        public Product()
+        {
+
+        }
+
+        public int ArtikelNr { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        public bool AvailableWhenSold { get; set; }
+        public Image ImgSrc { get; set; }
+    }
+}

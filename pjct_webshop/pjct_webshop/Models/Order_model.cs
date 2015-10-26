2e5e3b4275d5db6fbef3dae18b735a49767e0bdb
Erik Olofsson <anton.stjernman@gmail>
2015-10-26 16:09:34 +0100

    kurt

---
+++ pjct_webshop/pjct_webshop/Models/Order_model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pjct_webshop.Models
+{
+    public class Order_model
+    {
+        public List<Produkt_model> orderVaror { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string adress { get; set; }
+        public string stad { get; set; }
+        public string postNr { get; set; }
+        public string email { get; set; }
+        public string telefonNR { get; set; }
+        public string meddelande { get; set; }
+        public int Id { get; private set; }
+    }
+}

ee4cf23443da99edcce770a9ce8faad23a38abbb
Olle Svensson <aw.ots@outlook.com>
2015-10-27 13:19:51 +0100

    its going well! =)

--- pjct_webshop/pjct_webshop/Models/User.cs
+++ pjct_webshop/pjct_webshop/Models/User.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -15,4 +18,19 @@ namespace pjct_webshop.Models
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
+
+    public class UserLogic
+    {
+        string myConnection = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+        public int CheckUserLogin(User u)
+        {
+            using (SqlConnection conObj = new SqlConnection(myConnection))
+            {
+                SqlCommand myCommand = new SqlCommand("uspLogin", conObj);
+                myCommand.CommandType = CommandType.StoredProcedure;
+                myCommand.Parameters.Add(new SqlParameter("@UserName", UserId));
+
+            }
 }
+
+    }

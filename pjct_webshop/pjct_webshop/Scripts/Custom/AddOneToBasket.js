581b8ba10d2bbc4fa5988b051f99cef1767e7aa4
Cecilia Nilsson <c.e.l.nilsson@gmail.com>
2015-11-02 16:39:55 +0100

    ID på köp-knappar i alla produkt-views + ändrat "button" från "role" till "type"

--- pjct_webshop/pjct_webshop/Scripts/Custom/AddOneToBasket.js
+++ pjct_webshop/pjct_webshop/Scripts/Custom/AddOneToBasket.js
@@ -3,3 +3,11 @@
         alert$("#")
     });
 });
+
+
+$(document).ready(function () {
+    $("#AddNewProductButton").click(function () {
+        alert($("#NameNewProduct").val() + " " + $("#DescriptionNewProduct").val() + " " + $("#ImageNewProduct").val()
+            + " " + $("#NumbersLeftNewProduct").val() + " " + $("#PriceNewProduct").val());
+    });
+});
